e0d416f39c51f019ae83dfbd237e3c6c70e24c04
Pheeblyboo <Freak_del_fritz-88@live.nl>
2018-02-22 20:56:22 +0100

    setting up

---
+++ public/app.js
@@ -0,0 +1,11 @@
+// Get a reference to the database service
+/*let database = firebase.database();
+
+function writeRoosterData(userId, name, email, imageUrl) {
+  firebase.database().ref('users/' + userId).set({
+    username: name,
+    email: email,
+    profile_picture : imageUrl
+  });
+}*/
+
