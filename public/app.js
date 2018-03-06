a3d2d5a725c948b1ccc57f5ab61d0594b3eebf6b
Pheeblyboo <Freak_del_fritz-88@live.nl>
2018-03-06 17:50:59 +0100

    working version with Angular http and scope services

--- public/app.js
+++ public/app.js
@@ -7,39 +7,73 @@ function writeRoosterData(userId, name, email, imageUrl) {
     email: email,
     profile_picture : imageUrl
   });
-}*/
+};*/
 
-var app = angular.module('roosterApp', [])
-  app.controller('RoosterAddController', function($scope) {
-    $scope.master = {
-        maandag:
-        {
-        p1:
-        {naamfield:"Marissa", roosterUrl: "test", werkTijd: "9:00-17:00", pauze: "12:00"},
-        p2:
-        {naamfield:"Ruud", roosterUrl: "test", werkTijd: "9:00-17:00", pauze: "12:00"},
-        p3:
-        {naamfield:"Sascha", roosterUrl: "test", werkTijd: "9:00-17:00", pauze: "12:00"},
-        p4:
-        {naamfield:"Abel", roosterUrl: "test", werkTijd: "9:00-17:00", pauze: "12:00"},
-        p5:
-        {naamfield:"Ferdinand", roosterUrl: "test", werkTijd: "9:00-17:00", pauze: "12:00"}},
-        dinsdag:
-        {
-        p1:
-        {naamfield:"Marissa", roosterUrl: "test", werkTijd: "9:00-17:00", pauze: "12:00"},
-        p2:
-        {naamfield:"Ruud", roosterUrl: "test", werkTijd: "9:00-17:00", pauze: "12:00"},
-        p3:
-        {naamfield:"Sascha", roosterUrl: "test", werkTijd: "9:00-17:00", pauze: "12:00"},
-        p4:
-        {naamfield:"Abel", roosterUrl: "test", werkTijd: "9:00-17:00", pauze: "12:00"},
-        p5:
-        {naamfield:"Ferdinand", roosterUrl: "test", werkTijd: "9:00-17:00", pauze: "12:00"}},
-    };
+
+
+
+var app = angular.module('roosterApp', []);
+  app.controller('RoosterAddController', ['$scope', '$http', function($scope, $http) {
+
     var roosterList = this;
     roosterList.addcal = function() {
         $scope.master.push({naamfield, roosterUrl, werkTijd, pauze});
+        
     };
 
-  });
+  
+
+ /* app.controller('RoosterHttpGetController', ['$scope', function($scope) {*/
+    let myUrl = 'https://ogd.rooster.nl/InPlanningService/ICalService?key=ly65f52k0kwu51u6xqav5xb627an283x';
+    let proxy = 'https://cors-anywhere.herokuapp.com/';
+    function getIcalData() {
+        myUrl = document.getElementById("calUrl").value;
+        icalData();
+    }
+    function icalData() {
+        // The proxy url expects as first URL parameter the URL to be bypassed
+        // https://cors-anywhere.herokuapp.com/{my-url-to-bypass}
+        let req = {
+            method: 'GET',
+            url: proxy + myUrl,
+            'Content-Type': 'text/plain'
+        }; 
+        console.log(req.url);
+        $http(req)
+        .then(function(response) {
+            dataCal = response.data;
+            let jCalData = ICAL.parse(dataCal);
+            let comp = new ICAL.Component(jCalData);
+            let tevent = comp.getAllSubcomponents("vevent");
+            let calList = [];
+            let count = 0;
+            console.log(tevent);
+            for(const arrValue of tevent) {
+            let event = new ICAL.Event(arrValue);
+            let dump = {summary: event.summary, locatie: event.location, datum: event.startDate.day + "-" + event.startDate.month, starttijd: event.startDate.hour + ":"+ event.startDate.minute, eindtijd: event.endDate.hour + ":"+ event.endDate.minute };
+            
+            
+            if(dump.summary.match(/(BPD)|(FBNL)|(BIM)/g) !== null){
+                dump.locatie = dump.summary.match(/(BPD)|(FBNL)|(BIM)/g).join("");
+           };
+            if(dump.locatie.match(/Helvetios/g) !== null){
+                dump.locatie = "Utrecht SSD";
+           };
+            if(event.startDate.minute === 0){
+                dump.starttijd += "0";
+            }
+            if(event.endDate.minute === 0){
+                dump.eindtijd += "0";
+            }
+           
+        calList.push({id: count, data: dump});    
+        count++;
+               
+            } 
+        console.log(calList);
+        $scope.master = calList;
+        }), function(response) {console.log("Error"); };
+    }//)};
+icalData();
+}]);
+     // }]);
